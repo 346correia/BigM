9bb2574254ac9189af8b58a61a682b414fb0ca28
346correia <346correia@gmail.com>
2016-03-09 19:57:11 -0300

    Initial commit

---
+++ app/src/main/java/com/meudelivery/victorcorreia/bigm/MainActivity.java
@@ -0,0 +1,319 @@
+package com.meudelivery.victorcorreia.bigm;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+    private List<Categoria> listGroup;
+    private HashMap<Categoria, List<Item>> listData;
+    private List<Item> itemBd;
+    private List<Cliente> cliente;
+    private Button btnPedido;
+    private TextView txtNomeCliente;
+    private TextView txtEmailCliente;
+    private String nomeCliente;
+    private String emailCliente;
+
+    private ProgressDialog pd = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Big Mengão");
+        setSupportActionBar(toolbar);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //navigationView.getMenu().findItem(R.id.nav_entrar).setVisible(false);
+
+        View header = navigationView.getHeaderView(0);
+
+        txtNomeCliente = (TextView) header.findViewById(R.id.txtNomeCliente);
+        txtEmailCliente = (TextView) header.findViewById(R.id.txtEmailCliente);
+
+        BDCliente bdCliente = new BDCliente(this);
+        cliente = bdCliente.buscar();
+
+        if (cliente.isEmpty()) {
+            navigationView.getMenu().findItem(R.id.nav_logout).setVisible(false);
+            //navigationView.getMenu().findItem(R.id.nav_pedidos_anteriores).setVisible(false);
+
+        } else {
+            navigationView.getMenu().findItem(R.id.nav_entrar).setVisible(false);
+
+            nomeCliente = cliente.get(0).getNome();
+            emailCliente = cliente.get(0).getEmail();
+
+            txtNomeCliente.setText(nomeCliente);
+            txtEmailCliente.setText(emailCliente);
+        }
+
+        navigationView.getMenu().findItem(R.id.nav_sobre).setVisible(false);
+        navigationView.getMenu().findItem(R.id.nav_fale).setVisible(false);
+        navigationView.getMenu().findItem(R.id.nav_politica).setVisible(false);
+
+        btnPedido = (Button) findViewById(R.id.btnPedido);
+
+        btnPedido.setOnClickListener(this);
+
+        buildList();
+
+        //salvar lista
+        BDItem bdItem = new BDItem(this);
+
+        BDItemCore auxBd = new BDItemCore(this);
+
+        SQLiteDatabase db = auxBd.getWritableDatabase();
+        db.execSQL("DELETE FROM item"); //delete all rows in a table
+        //db.close();
+
+        for (int i = 0; i < itemBd.size(); i++) {
+            bdItem.inserir(itemBd.get(i));
+        }
+
+
+        final ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        expandableListView.setAdapter(new ExpandableAdapter(MainActivity.this, listGroup, listData));
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+
+                Intent it = new Intent(MainActivity.this, TelaAddAoPedido.class);
+
+                it.putExtra("IDITEM", (int) id);
+
+                startActivity(it);
+                //Toast.makeText(MainActivity.this, "Group: " + groupPosition + " | Item:" + id, Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                //Toast.makeText(MainActivity.this, "Group (Expand): " + groupPosition, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                //Toast.makeText(MainActivity.this, "Group (Collapse): " + groupPosition, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    public void buildList() {
+
+        listGroup = new ArrayList<Categoria>();
+        listData = new HashMap<Categoria, List<Item>>();
+
+        //Categorias
+        AcessoWS ar = new AcessoWS();
+
+        String chamadaWS;
+
+        chamadaWS = "http://webservicevictor.16mb.com/android/get_all_categoria.php";
+
+        String resultado = ar.chamadaGet(chamadaWS);
+
+        try {
+            Gson g = new Gson();
+
+            Type categoriaType = new TypeToken<List<Categoria>>() {
+            }.getType();
+
+            listGroup = (List<Categoria>) g.fromJson(resultado, categoriaType);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast toast = Toast.makeText(this, "Erro", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        //Itens
+        chamadaWS = "http://webservicevictor.16mb.com/android/get_all_item.php";
+
+        resultado = ar.chamadaGet(chamadaWS);
+
+        try {
+            Gson g = new Gson();
+
+            //private List<Item> it;
+
+            Type categoriaType = new TypeToken<List<Item>>() {
+            }.getType();
+
+            itemBd = (List<Item>) g.fromJson(resultado, categoriaType);
+            //itemRealm = (List<ItemRealm>) g.fromJson(resultado, categoriaType);
+
+            for (int i = 0; i < listGroup.size(); i++) {
+
+                List<Item> auxList = new ArrayList<Item>();
+
+                for (int n = 0; n < itemBd.size(); n++) {
+                    //Item
+                    if (itemBd.get(n).getIdCategoria() == i + 1) {
+                        auxList.add(itemBd.get(n));
+                    }
+                }
+                listData.put(listGroup.get(i), auxList);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast toast = Toast.makeText(this, "Erro", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            //super.onBackPressed();
+            Intent a = new Intent(Intent.ACTION_MAIN);
+            a.addCategory(Intent.CATEGORY_HOME);
+            a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(a);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu){
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        item.setVisible(false);
+
+        return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_entrar) {
+
+            startActivity(new Intent(this, TelaLogin.class));
+            //finish();
+
+        } else if (id == R.id.nav_logout) {
+
+            BDClienteCore auxCliCore = new BDClienteCore(this);
+            SQLiteDatabase db = auxCliCore.getWritableDatabase();
+            db.execSQL("DELETE FROM cliente");
+
+            LoginManager.getInstance().logOut();
+
+            new MyAsyncTask().execute();
+            finish();
+
+        } else if (id == R.id.nav_sobre) {
+
+        } else if (id == R.id.nav_fale) {
+
+        } else if (id == R.id.nav_politica) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent it = new Intent(this, TelaPedido.class);
+        startActivity(it);
+
+    }
+
+    private class MyAsyncTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setTitle("Aguarde");
+            pd.setMessage("Carregando...");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Intent it = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(it);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+            pd.dismiss();
+        }
+
+
+    }
+
+}
